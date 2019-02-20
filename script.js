2f7de697fb80a0b84b325051ded2438e74e5f918
wonjongOh94 <jgyuity1289@gmail.com>
2019-02-21 01:19:34 +0900

    20190220 commit

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const scrollDown = function () {
-    $("html, body").animate({
-        scrollTop: $("#secondPage").offset.top
-    }, 1000);
-    return false;
+const scrollDown = function (n) {
+    if(n === 1) {
+        $("html, body").animate({
+            scrollTop: $("#secondPage").offset().top
+        }, 1000);
+        return false;
+    } else if(n === 2) {
+        $("html, body").animate({
+            scrollTop: $("#thirdPage").offset().top
+        }, 1000);
+        return false; 
+    } else if(n === 3) {
+        $("html, body").animate({
+            scrollTop: $("#fourthPage").offset().top
+        }, 1000);
+        return false; 
+    } else if(n === 4) {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1500);
+        return false; 
+    }
 }
